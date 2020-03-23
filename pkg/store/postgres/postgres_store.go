41a3e83666c2f1ff963fe5c6d81e6e2b057a2226
Daniel Ribeiro <drgomesp@gmail.com>
2020-03-23 09:15:41 -0300

    Implemented read only for postgres store (persist mode, generate table name)

--- pkg/store/postgres/postgres_store.go
+++ pkg/store/postgres/postgres_store.go
@@ -2,50 +2,84 @@ package postgres
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/happendb/happendb/pkg/messaging"
+	"github.com/happendb/happendb/pkg/store"
 	_ "github.com/lib/pq"
 )
 
+// StoreOption ...
+type StoreOption = func(*Store)
+
+// WithPersistMode ...
+func WithPersistMode(m store.PersistMode) StoreOption {
+	return func(s *Store) {
+		s.persistMode = m
+	}
+}
+
 // Store ...
 type Store struct {
-	db *sql.DB
+	db          *sql.DB
+	persistMode store.PersistMode
 }
 
 // NewPostgresStore ...
-func NewPostgresStore() (*Store, error) {
+func NewPostgresStore(opts ...StoreOption) (*Store, error) {
 	db, err := sql.Open("postgres", "host=localhost dbname=happendb user=postgres password=123 sslmode=disable")
 
 	if err != nil {
 		return nil, err
 	}
 
-	return &Store{
-		db,
-	}, nil
+	store := &Store{
+		db: db,
+	}
+
+	for _, opt := range opts {
+		opt(store)
+	}
+
+	return store, nil
 }
 
 // ReadEvents ...
-func (s *Store) ReadEvents(aggregateID string) ([]*messaging.Event, error) {
+func (s *Store) ReadEvents(aggregateID string) (*messaging.EventStream, error) {
 	var (
-		err    error
-		rows   *sql.Rows
-		events = make([]*messaging.Event, 0)
+		err       error
+		rows      *sql.Rows
+		tableName string
 	)
 
-	if rows, err = s.db.Query("SELECT * FROM events WHERE aggregate_id = $1", aggregateID); err != nil {
+	if tableName, err = generateTableName(s.persistMode, aggregateID); err != nil {
 		return nil, err
 	}
+
+	if rows, err = s.db.Query(fmt.Sprintf("SELECT * FROM %s WHERE aggregate_id = $1", tableName), aggregateID); err != nil {
+		return nil, err
+	}
+
+	stream := messaging.NewEventStream(aggregateID)
 
 	for rows.Next() {
 		event := messaging.NewEvent()
 
-		if err := rows.Scan(&event.Id, &event.Type, &event.AggregateID, &event.Payload.Value); err != nil {
+		if err := rows.Scan(&event.Id, &event.Type, &event.AggregateId, &event.Payload.Value); err != nil {
 			return nil, err
 		}
 
-		events = append(events, event)
+		stream.Append(event)
 	}
 
-	return events, nil
+	return stream, nil
+}
+
+func generateTableName(persistMode store.PersistMode, _ string) (string, error) {
+	switch persistMode {
+	case store.PersistModeSingleTable:
+		return "events", nil
+	default:
+		return "", store.ErrInvalidTableName
+	}
 }
--- pkg/store/store.go
+++ pkg/store/store.go
@@ -1,8 +1,25 @@
 package store
 
-import "github.com/happendb/happendb/proto/gen/go/messaging"
+import (
+	"errors"
+
+	"github.com/happendb/happendb/pkg/messaging"
+)
+
+// PersistMode ...
+type PersistMode = int
+
+const (
+	// PersistModeSingleTable ...
+	PersistModeSingleTable PersistMode = iota
+)
+
+var (
+	// ErrInvalidTableName ...
+	ErrInvalidTableName = errors.New("invalid table name")
+)
 
 // ReadOnlyStore ...
 type ReadOnlyStore interface {
-	ReadEvents(aggregateID string) ([]*messaging.Event, error)
+	ReadEvents(aggregateID string) (*messaging.EventStream, error)
 }
