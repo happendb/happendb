84c5c7b7e7204126b40df2433a2d7e40f01050ee
Daniel Ribeiro <drgomesp@gmail.com>
2020-03-23 09:15:56 -0300

    Implemented read only at the server with some basic debugging

--- internal/server/read_only.go
+++ internal/server/read_only.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"net"
 
+	"github.com/happendb/happendb/pkg/messaging"
 	"github.com/happendb/happendb/pkg/store"
 	"github.com/happendb/happendb/pkg/store/postgres"
 	pb "github.com/happendb/happendb/proto/gen/go/store"
+	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 )
 
@@ -18,7 +20,7 @@ type ReadOnlyServer struct {
 
 // NewReadOnlyServer ...
 func NewReadOnlyServer() (*ReadOnlyServer, error) {
-	store, err := postgres.NewPostgresStore()
+	store, err := postgres.NewPostgresStore(postgres.WithPersistMode(store.PersistModeSingleTable))
 
 	if err != nil {
 		return nil, err
@@ -35,8 +37,8 @@ func NewReadOnlyServer() (*ReadOnlyServer, error) {
 }
 
 // Run ...
-func (s *ReadOnlyServer) Run() error {
-	lis, err := net.Listen("tcp", ":1232")
+func (s ReadOnlyServer) Run() error {
+	lis, err := net.Listen("tcp", ":3000")
 
 	if err != nil {
 		return err
@@ -46,15 +48,28 @@ func (s *ReadOnlyServer) Run() error {
 }
 
 // ReadEvents ...
-func (s *ReadOnlyServer) ReadEvents(ctx context.Context, req *pb.ReadEventsRequest) (*pb.ReadEventsResponse, error) {
-	events, err := s.store.ReadEvents(req.GetAggregateID())
+func (s ReadOnlyServer) ReadEvents(ctx context.Context, req *pb.ReadEventsRequest) (*pb.ReadEventsResponse, error) {
+	var (
+		err    error
+		stream *messaging.EventStream
+		events []*messaging.Event
+	)
 
-	if err != nil {
+	if stream, err = s.store.ReadEvents(req.GetAggregateID()); err != nil {
 		return nil, err
 	}
 
+	for event := range stream.Events() {
+		events = append(events, event)
+	}
+
+	log.WithFields(log.Fields{
+		"stream_name":   stream.Name(),
+		"stream_length": stream.Len(),
+	}).Debugf("%T::ReadEvents(%#v)\n", s, req)
+
 	return &pb.ReadEventsResponse{
-		AggregateID: req.GetAggregateID(),
+		AggregateId: req.GetAggregateID(),
 		Events:      events,
 	}, nil
 }
