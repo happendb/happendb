4130ca258b451e6bbfcd548c99aa4d4af83c9568
Daniel Ribeiro <drgomesp@gmail.com>
2020-03-23 09:14:44 -0300

    Created event stream abstraction

---
+++ pkg/messaging/event_stream.go
@@ -0,0 +1,47 @@
+package messaging
+
+import "github.com/happendb/happendb/proto/gen/go/messaging"
+
+// EventStream ...
+type EventStream struct {
+	name   string
+	events []*Event
+}
+
+// NewEventStream ...
+func NewEventStream(name string) *EventStream {
+	return &EventStream{
+		name,
+		make([]*Event, 0),
+	}
+}
+
+// Append ...
+func (s *EventStream) Append(e ...*messaging.Event) {
+	s.events = append(s.events, e...)
+}
+
+// Name ...
+func (s EventStream) Name() string {
+	return s.name
+}
+
+// Len ...
+func (s EventStream) Len() int {
+	return len(s.events)
+}
+
+// Events ...
+func (s EventStream) Events() chan *Event {
+	ch := make(chan *Event)
+
+	go func() {
+		defer close(ch)
+
+		for _, e := range s.events {
+			ch <- e
+		}
+	}()
+
+	return ch
+}
