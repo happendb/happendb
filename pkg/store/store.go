05f62043cf981b1bc36cc5af2ceeebd3c0c49161
Daniel Ribeiro <drgomesp@gmail.com>
2020-04-01 11:26:58 -0300

    Added invalid stream name error function

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -2,8 +2,8 @@ package store
 
 import (
 	"errors"
+	"fmt"
 
-	"github.com/happendb/happendb/internal/logtime"
 	pbMessaging "github.com/happendb/happendb/proto/gen/go/happendb/messaging/v1"
 )
 
@@ -18,18 +18,22 @@ const (
 var (
 	// ErrInvalidTableName ...
 	ErrInvalidTableName = errors.New("invalid table name")
+	// ErrInvalidStreamName ...
+	ErrInvalidStreamName = func(name string) error {
+		return fmt.Errorf("invalid stream name '%v'", name)
+	}
 	// ErrExpectedVersion ...
 	ErrExpectedVersion = errors.New("expected version should be current version + 1")
 )
 
 // AsyncReaderStore ...
 type AsyncReaderStore interface {
-	ReadStreamEventsForwardAsync(streamName string, offset uint64, limit uint64) (<-chan *pbMessaging.Event, error)
+	ReadEventsForwardAsync(streamName string, offset uint64, limit uint64) (<-chan *pbMessaging.Event, error)
 }
 
 // SyncReaderStore ...
 type SyncReaderStore interface {
-	ReadStreamEventsForward(streamName string, offset uint64, limit uint64) ([]*pbMessaging.Event, error)
+	ReadEventsForward(streamName string, offset uint64, limit uint64) ([]*pbMessaging.Event, error)
 }
 
 // ReaderStore ...
@@ -41,77 +45,4 @@ type ReaderStore interface {
 // WriteOnlyStore ...
 type WriteOnlyStore interface {
 	Append(streamName string, events ...*pbMessaging.Event) error
-}
-
-// PersistentStoreOption ...
-type PersistentStoreOption = func(*PersistentStore)
-
-// WithDriver ...
-func WithDriver(d Driver) PersistentStoreOption {
-	return func(s *PersistentStore) {
-		s.driver = d
-	}
-}
-
-// WithPersistMode ...
-func WithPersistMode(m PersistMode) PersistentStoreOption {
-	return func(s *PersistentStore) {
-		s.persistMode = m
-	}
-}
-
-// PersistentStore ...
-type PersistentStore struct {
-	driver      Driver
-	persistMode PersistMode
-}
-
-// NewPersistentStore ...
-func NewPersistentStore(opts ...PersistentStoreOption) (*PersistentStore, error) {
-	store := &PersistentStore{
-		persistMode: PersistModeSingleTable,
-	}
-
-	for _, opt := range opts {
-		opt(store)
-	}
-
-	return store, nil
-}
-
-// Append ...
-func (s *PersistentStore) Append(streamName string, events ...*pbMessaging.Event) error {
-	defer logtime.Elapsedf("%T::Append", s)()
-
-	exists, err := s.driver.HasStream(streamName)
-
-	if err != nil {
-		return err
-	}
-
-	if !exists {
-		if _, err := s.driver.CreateStream(streamName); err != nil {
-			return err
-		}
-	}
-
-	return s.driver.Append(streamName, events...)
-}
-
-// ReadStreamEventsForward ...
-func (s *PersistentStore) ReadStreamEventsForward(streamName string, offset uint64, limit uint64) (events []*pbMessaging.Event, err error) {
-	defer logtime.Elapsedf("%T::ReadStreamEventsForward", s)()
-
-	events, err = s.driver.ReadStreamEventsForward(streamName, offset, limit)
-
-	return
-}
-
-// ReadStreamEventsForwardAsync ...
-func (s *PersistentStore) ReadStreamEventsForwardAsync(streamName string, offset uint64, limit uint64) (eventsCh <-chan *pbMessaging.Event, err error) {
-	defer logtime.Elapsedf("%T::ReadStreamEventsForwardAsync", s)()
-
-	eventsCh, err = s.driver.ReadStreamEventsForwardAsync(streamName, offset, limit)
-
-	return
 }
